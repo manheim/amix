aff8facc3b88103bedc293f5be25b0fd844e995c
Nicotine Free Coder <ifspair+paul@gmail.com>
2017-08-23 17:40:39 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugNewrelic.Mixfile do
 
   def project do
     [app: :plug_newrelic,
-     version: "0.0.7",
+     version: "0.0.8",
      elixir: "~> 1.0",
      deps: deps(),
      package: package(),
